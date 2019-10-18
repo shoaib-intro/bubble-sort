7e52e7069656d92ed3dd4ff30d24031106e8a4a2
Shoaib Ali <56737996+shoaib-intro@users.noreply.github.com>
2019-10-18 23:48:43 +0200

    Program

--- program.cs
+++ program.cs
@@ -1,1 +1,27 @@
 
+            bool swapped;
+            int[] input=new int[] {54321};
+            int temp;
+            for (int i = 0; i < input.Length-1;i++ )
+            {
+                swapped = false;
+                for (int j = 0; j < input.Length-i-1; j++)
+                {
+                    if (input[j + 1] < input[j])
+                    {
+                        temp = input[j];
+                        input[j] = input[j + 1];
+                        input[j + 1] = input[j];
+                        swapped = true;
+                        Console.WriteLine($"{input[j]}");
+                    }
+
+                    
+                }
+                if (swapped == false)
+                    break;
+            }
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.WriteLine($"Sorted Array is {input[i]}");
+            }
